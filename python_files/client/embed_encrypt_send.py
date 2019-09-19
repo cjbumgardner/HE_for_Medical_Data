96e08027f69154cfafdd4a71974568d15432aab3
Christopher Bumgardner <cjbumgardner@gmail.com>
2019-09-18 22:31:10 -0700

    first commit

---
+++ python_files/client/embed_encrypt_send.py
@@ -0,0 +1,31 @@
+"""
+Input:
+yml
+    model_type: 
+    security_level:
+    bit_precision: (e.g. int32)
+ndarray
+    data: 
+
+Output:
+ndarray 
+    data_out: preprocessed and encrypted data, serialized 
+seal pk datatype
+    pk: public key 
+
+"""
+
+class client_side_processing(object):
+    """
+    
+    """
+    def __init__(self,
+                data,
+                path_to_model
+                ):
+        self.data = data
+        self.model = 
+
+    def 
+    
+
---
+++ python_files/models/log_reg.py
@@ -0,0 +1,8 @@
+"""
+Train a logistic regression model. 
+    Input: 
+        data: 
+    Output: 
+        params: parameters of logistic regression model
+"""
+
